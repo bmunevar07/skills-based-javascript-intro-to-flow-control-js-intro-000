dba645e04a26e1f665ff5940030b261823a46421
Brian M <b29munevar@gmail.com>
2019-02-18 21:21:13 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,6 +1,15 @@
 function basicTeenager(age) {
   return("You are a teenager!");
-  
+  it('should return "You are a teenager!" if the age is between 13-19', () => {
+      expect(basicTeenager(13)).toEqual("You are a teenager!");
+    })
+
+    it('should return undefined if the age is not between 13-19', () => {
+      expect(basicTeenager(12)).toBe(undefined)
+    })
+
+  })
+
 }
 
 function teenager(age) {
