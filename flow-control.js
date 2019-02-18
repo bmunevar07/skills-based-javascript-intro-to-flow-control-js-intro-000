c7e57b592a07ffd12b4bf17c9be28ba63318d0cf
Brian M <b29munevar@gmail.com>
2019-02-18 21:18:41 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -3,11 +3,11 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-  return("You are a teenager!");
+  return("You are not a teenager!");
 }
 
 function ageChecker(age) {
-  return ("You are not a teenager!");
+  return ("You are a teenager!");
 }
 
 function ternaryTeenager(age) {
